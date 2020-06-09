0d18fe592f06836a5a556d0f663142131cc4307a
Pablo <pablo.s.perez@hotmail.com>
2020-06-08 21:53:50 -0300

    primer commit

---
+++ src/main/java/ar/edu/davinci/dvds20201cg1/DvDs20201cG1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvDs20201cG1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DvDs20201cG1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/davinci/dvds20201cg1/DvDs20201cG1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.davinci.dvds20201cg1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvDs20201cG1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
